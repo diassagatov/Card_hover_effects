3a327a18834fd56c756ba621d9e213d3a1f29f38
Dias Sagatov <diassagatov02@gmail.com>
2023-06-04 03:10:05 +0500

    Upd 1.1

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-document.getElementById("container").onmousemove = e =>{
-    for(const card of document.getElementsByClassName("card")){
+document.getElementById("container").onmousemove = e => {
+    for (const card of document.getElementsByClassName("card")) {
         const rect = card.getBoundingClientRect(),
-        x = e.clientX - rect.left,
-        y = e.clientY - rect.top;
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
 
         card.style.setProperty("--mouse-x", `${x}px`);
         card.style.setProperty("--mouse-y", `${y}px`);
-    }  
+    }
 }
 
